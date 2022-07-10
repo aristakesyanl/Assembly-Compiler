629d160146f4dcd3edf20fedc5ef0e6b68dfd4f3
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-07-10 19:59:57 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,15 @@
-
+#include"AssemblyCompiler.h"
+#include<iostream>
+int main(int argc, char* argv[])
+{
+	if(argc<2)
+	{
+		std::cerr<<"Not enough arguments are provided"<<std::endl;
+		return 1;
+	}
+	else{
+		std::string str(argv[1]);
+		AssemblyCompiler compiler(str);
+		compiler.binFileInit();
+	}
+}
