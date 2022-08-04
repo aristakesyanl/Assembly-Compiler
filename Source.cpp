98eb2471696b9141cdc33f74917918372e69f47c
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-08-04 18:01:55 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,15 +2,20 @@
 #include<iostream>
 int main(int argc, char* argv[])
 {
-	if(argc<2)
-	{
-		std::cerr<<"Not enough arguments are provided"<<std::endl;
-		return 1;
-	}
-	else{
-		std::string str(argv[1]);
-		AssemblyCompiler compiler(str);
-		compiler.binFileInit();
-		return 0;
-	}
+    if(argc<2)
+    {
+        std::cerr<<"Not enough arguments are provided"<<std::endl;
+        return 1;
+    }
+    else{
+        std::string str(argv[1]);
+        AssemblyCompiler compiler(str);
+        int state=compiler.binFileInit();
+        if(state==0){
+            std::cout<<"Success!!"<<std::endl;
+        }
+        else{
+            std::cerr<<"Failed to create binary file"<<std::endl;
+        }
+    }
 }
